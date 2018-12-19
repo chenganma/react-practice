005f98543295d6f16d380729282b50bd063c1d7b
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2018-12-19 16:24:57 -0600

    complete file of App

--- practice02/App.js
+++ practice02/App.js
@@ -1,2 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom"
+
+import Header from "./components/Header"
+import MainContent from "./components/MainContent"
+import Footer from "./components/Footer"
+
+
+function App(){
+
+	return (
+		<div>
+		   <Header />
+		   <MainContent />
+		   <Footer />
+		</div>
+	)
+
+}
+
+export default App
