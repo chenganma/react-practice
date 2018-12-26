de5d365450f34618150914823effd5b3291c15d0
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2018-12-25 19:17:02 -0600

    to do list part 2

---
+++ practice05/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import TodoItem from "./TodoItem"
+
+function App() {
+    return (
+        <div className="tofo-item">
+        	<TodoItem />
+        	<TodoItem />
+        	<TodoItem />
+        	<TodoItem />
+        </div>
+    )
+}
+
+export default App
