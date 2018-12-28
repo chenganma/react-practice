e62df49a3d20765eb93b2a0c88ecdff58727be4b
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2018-12-27 23:08:51 -0600

    add one line comment

--- my-app/App.js
+++ my-app/App.js
@@ -1,6 +1,9 @@
 //import React, { Component } from 'react'
 //import logo from './logo.svg'
 //import './App.css'
+
+//mapping components
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
