f238d60ca6c22a0c89c6f3cb3863fe9d459b7880
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2019-01-02 23:17:42 -0600

    try style and looking for new idea

--- practice06/Product.js
+++ practice06/Product.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+const divStyle = {
+  fontSize: '15px',
+  textAlign: 'center'
+};
+
 function Product(props){
 	return(
-      <div>
+      <div style={divStyle}>
 		<h2>{props.product.name}</h2>
-		<p>${props.product.price}.00  -  {props.product.description}</p>
-		<hr/>
+		<img src="https://*****/imagedemo/demo01.png" />
+		<p>{props.product.price}  {props.product.description}</p>
+		
 	  </div>
 	)
 }
