febc0c6ac6054ccf7dd16432c47b3c54d787cbbc
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2019-01-03 22:24:43 -0600

    new url

--- practice06/Product.js
+++ practice06/Product.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 const divStyle = {
   fontSize: '15px',
+  width: '450px',
   textAlign: 'center'
+};
+
+const h2Style = {
+  fontSize: '15px',
+  textAlign: 'left'
 };
 
 function Product(props){
 	return(
       <div style={divStyle}>
-		<h2>{props.product.name}</h2>
-		<img src="https://*****/imagedemo/demo01.png" />
+		<h2 style={h2Style}>{props.product.name}</h2>
+		<img src="https://placekitten.com/350/350" />
 		<p>{props.product.price}  {props.product.description}</p>
 		
 	  </div>
